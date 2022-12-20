362fe0f5f771d4b97f4043b29ecc1e4852b86d08
Denis Knor <knorred@mail.ru>
2022-12-20 11:30:54 +0300

    fix: js review

--- script/index.js
+++ script/index.js
@@ -3,16 +3,16 @@ const popupCloseButtonElement = popupElement.querySelector('.popup__close-button
 const popupSaveButtonElement = popupElement.querySelector('.popup__save-button');
 const popupOpenButtonElement = document.querySelector('.profile__edit-button');
 let likeButton = document.querySelector('.element__like-button');
+let profileName = document.querySelector('.profile__name');
+let profileDescription = document.querySelector('.profile__description');
+let popupName = popupElement.querySelector('.popup__text_name');
+let popupDescription = popupElement.querySelector('.popup__text_description');
 
 function openPopup() {
     popupElement.classList.add('popup_is-opened');
-    let profileName = document.querySelector('.profile__name');
-    let profileDescription = document.querySelector('.profile__description');
-    console.log(profileName.textContent);
-    console.log(profileDescription.textContent);
-    let popupName = popupElement.querySelector('.popup__text_name');
+    //console.log(profileName.textContent);
+    //console.log(profileDescription.textContent);
     popupName.value = profileName.textContent;
-    let popupDescription = popupElement.querySelector('.popup__text_description');
     popupDescription.value = profileDescription.textContent;
 }
 const closePopup = function () {
@@ -20,29 +20,29 @@ const closePopup = function () {
 }
 
 const savePopup = function () {
-    let popupName = popupElement.querySelector('.popup__text_name');
-    let popupDescription = popupElement.querySelector('.popup__text_description');
-    if (popupName.value.length < 2) {
-        alert('длина имени должна быть больше 3х символов');
-    } else if (popupDescription.value.length < 2) {
-        alert('длина подписи должна быть больше 3х символов');
-    } else {
-        console.log(popupName.value.length);
-        let profileName = document.querySelector('.profile__name');
-        let profileDescription = document.querySelector('.profile__description');
+    //let popupName = popupElement.querySelector('.popup__text_name');
+    //let popupDescription = popupElement.querySelector('.popup__text_description');
+    //if (popupName.value.length < 2) {
+    //    alert('длина имени должна быть больше 3х символов');
+   // } else if (popupDescription.value.length < 2) {
+   //     alert('длина подписи должна быть больше 3х символов');
+   // } else {
+   //     console.log(popupName.value.length);
+    //    let profileName = document.querySelector('.profile__name');
+    //    let profileDescription = document.querySelector('.profile__description');
         profileName.textContent = popupName.value;
         profileDescription.textContent = popupDescription.value;
         closePopup();
         return;
-    }
+   // }
 }
 
-const likeActivate = function (event) {
-    console.log(event.target);
-    console.log(likeButton);
-    likeButton = event.target;
-    likeButton.classList.toggle('element__like-button_is-liked');
-}
+//const likeActivate = function (event) {
+//    console.log(event.target);
+//    console.log(likeButton);
+//    likeButton = event.target;
+//    likeButton.classList.toggle('element__like-button_is-liked');
+//}
 const closePopupByClickOverlay = function (event) {
     if (event.target === event.currentTarget) {
         closePopup();
@@ -53,5 +53,5 @@ const closePopupByClickOverlay = function (event) {
 popupOpenButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
 popupSaveButtonElement.addEventListener('click', savePopup);
-likeButton.addEventListener('click', likeActivate);
+//likeButton.addEventListener('click', likeActivate);
 popupElement.addEventListener('click', closePopupByClickOverlay);
