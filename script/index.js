dcc6b91ac07fc9dc97eee40764b6d4f92fe9df58
Denis Knor <knorred@mail.ru>
2023-01-12 15:41:42 +0300

    feat: add cards from js

--- script/index.js
+++ script/index.js
@@ -33,6 +33,45 @@ const closePopupByClickOverlay = function (event) {
     }
 }
 
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+initialCards.forEach(item => addCard(item.name, item.link));
+
+    function addCard(cardName, cardLink) {
+        const tempElement = document.querySelector('.temp-element').content;
+        const elements = document.querySelector('.elements');
+        const newElement = tempElement.querySelector('.element').cloneNode(true);
+        newElement.querySelector('.element__name').textContent = cardName;
+        newElement.querySelector('.element__img').src = cardLink;
+        newElement.querySelector('.element__img').alt = cardName;
+        elements.prepend(newElement);
+    };
+
 popupOpenButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
 popupForm.addEventListener('submit', savePopup);
