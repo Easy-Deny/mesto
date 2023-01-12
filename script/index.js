44d569c8b4ec69f1bec54853fd40249ce4cfa0d7
Denis Knor <knorred@mail.ru>
2023-01-12 17:51:15 +0300

    feat: add new popup and functional

--- script/index.js
+++ script/index.js
@@ -2,36 +2,62 @@ const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
 const popupForm = popupElement.querySelector('.popup__content');
 const popupOpenButtonElement = document.querySelector('.profile__edit-button');
-let likeButton = document.querySelector('.element__like-button');
+
+const addPopupElement = document.querySelector('.popup_type_add-card');
+const addPopupOpenButtonElement = document.querySelector('.profile__add-button');
+const addPopupCloseButtonElement = addPopupElement.querySelector('.popup__close-button');
+const addPopupForm = addPopupElement.querySelector('.popup__content');
+let addPopupName = addPopupElement.querySelector('.popup__text_type_name');
+let addPopupLink = addPopupElement.querySelector('.popup__text_type_description');
+
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
 let popupName = popupElement.querySelector('.popup__text_type_name');
 let popupDescription = popupElement.querySelector('.popup__text_type_description');
 
-function openPopup() {
+const openPopup = function() {
     popupElement.classList.add('popup_is-opened');
     popupName.value = profileName.textContent;
     popupDescription.value = profileDescription.textContent;
 }
+
+const openAddPopup = function() {
+    addPopupElement.classList.add('popup_is-opened');
+    addPopupName.value='';
+    addPopupLink.value='';
+}
+const closeAddPopup = function () {
+    addPopupElement.classList.remove('popup_is-opened');
+}
 const closePopup = function () {
     popupElement.classList.remove('popup_is-opened');
 }
-
 const savePopup = function (evt) {
-
     evt.preventDefault();
     profileName.textContent = popupName.value;
     profileDescription.textContent = popupDescription.value;
     closePopup();
     return;
-
+}
+const saveAddPopup = function (evt) {
+    evt.preventDefault();
+    addCard(addPopupName.value, addPopupLink.value);
+    closeAddPopup();
+    return;
 }
 
 const closePopupByClickOverlay = function (event) {
     if (event.target === event.currentTarget) {
         closePopup();
+        closeAddPopup();
     }
 }
+
+//const likeButtonLiked =  function(evt){
+//    console.log(evt.target);
+//    const likeButtonTarget = evt.target;
+//    likeButtonTarget.classList.add('element__like-button_is-liked');
+//};
 
 const initialCards = [
     {
@@ -62,17 +88,24 @@ const initialCards = [
 
 initialCards.forEach(item => addCard(item.name, item.link));
 
-    function addCard(cardName, cardLink) {
-        const tempElement = document.querySelector('.temp-element').content;
-        const elements = document.querySelector('.elements');
-        const newElement = tempElement.querySelector('.element').cloneNode(true);
-        newElement.querySelector('.element__name').textContent = cardName;
-        newElement.querySelector('.element__img').src = cardLink;
-        newElement.querySelector('.element__img').alt = cardName;
-        elements.prepend(newElement);
-    };
+function addCard(cardName, cardLink) {
+    const tempElement = document.querySelector('.temp-element').content;
+    const elements = document.querySelector('.elements');
+    const newElement = tempElement.querySelector('.element').cloneNode(true);
+    newElement.querySelector('.element__name').textContent = cardName;
+    newElement.querySelector('.element__img').src = cardLink;
+    newElement.querySelector('.element__img').alt = cardName;
+    newElement.querySelector('.element__like-button').addEventListener('click', function (evt) {
+    evt.target.classList.toggle('element__like-button_is-liked');
+    });
+    elements.prepend(newElement);
+};
 
 popupOpenButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
 popupForm.addEventListener('submit', savePopup);
 popupElement.addEventListener('click', closePopupByClickOverlay);
+addPopupElement.addEventListener('click', closePopupByClickOverlay);
+addPopupOpenButtonElement.addEventListener('click', openAddPopup)
+addPopupCloseButtonElement.addEventListener('click', closeAddPopup);
+addPopupForm.addEventListener('submit', saveAddPopup);
