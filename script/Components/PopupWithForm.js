89b43c408355f81b4591fefc2e41668e337f7ec2
Denis Knor <knorred@mail.ru>
2023-03-16 19:40:36 +0300

    fix: delete eventListener on closePopup

--- script/Components/PopupWithForm.js
+++ script/Components/PopupWithForm.js
@@ -1,56 +1,48 @@
-import {Popup} from "./Popup.js";
+import { Popup } from "./Popup.js";
 import { openedPopupSelector } from "../Utils/constants.js";
 
-export class PopupWithForm extends Popup{
-    constructor(popupSelector, submitForm, data){
+export class PopupWithForm extends Popup {
+    constructor(popupSelector, submitForm, data) {
         super(popupSelector);
         this._handleEscClose = this._handleEscClose.bind(this);
         this.setEventListeners = this.setEventListeners.bind(this);
-        this._popupSelector  = popupSelector;
-        this._submitForm= submitForm;
+        this._popupSelector = popupSelector;
+        this._submitForm = submitForm;
         this.openPopup = this.openPopup.bind(this);
         this._popup = document.querySelector(`.${popupSelector}`);
         this._addCardForm = this._popup.querySelector('.popup__content');
-        
+        this._getInputValues = this._getInputValues.bind(this);
 
     }
 
 
-    _getInputValues(){
-        //this._addPopupFormName = this._popup.querySelector('.popup__text_type_name');
-        //this._addPopupFormDescription = this._popup.querySelector('.popup__text_type_description');
-        //this.name = this._addPopupFormName.value;
-        //this.description = this._addPopupFormDescription.value;
-       // this.name = this._popup.querySelector('.popup__text_type_name').value;
-       // this.description = this._popup.querySelector('.popup__text_type_description').value;
+    _getInputValues() {
         this.item = {
             name: this._popup.querySelector('.popup__text_type_name').value,
             description: this._popup.querySelector('.popup__text_type_description').value
-        }  
+        }
         return this.item;
     }
-    setEventListeners(){
+    setEventListeners() {
         this._popup.addEventListener('mousedown', (evt) => {
-        if (evt.target.classList.contains(openedPopupSelector)) {
-            this.closePopup()
-        }
-        if (evt.target.classList.contains('popup__close-button')) {
-            this.closePopup();
-        }
-    });
-
-    //console.log(this._getInputValues());
-    //console.log(this._addCardForm);
-    this._addCardForm.addEventListener('submit', (evt)=>{
+            if (evt.target.classList.contains(openedPopupSelector)) {
+                this.closePopup()
+            }
+            if (evt.target.classList.contains('popup__close-button')) {
+                this.closePopup();
+            }
+        });
+        this._addCardForm.addEventListener('submit', this._submit)
+    }
+    closePopup() {
+        this._popup.classList.remove(openedPopupSelector);
+        document.removeEventListener('keydown', this._handleEscClose);
+        this._addCardForm.removeEventListener('submit', this._submit);
+        this._addCardForm.reset();
+    }
+    _submit(evt) {
+        console.log('push');
         evt.preventDefault();
-        //this.func=this._getInputValues();
-        //console.log(this.func);
-       this._submitForm(this._getInputValues());
-    });
-}
-closePopup() {
-    this._popup.classList.remove(openedPopupSelector);
-    document.removeEventListener('keydown', this._handleEscClose);
-    this._addCardForm.reset();
-}
+        this._submitForm(this._getInputValues());
+    }
 }
